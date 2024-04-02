75bf71b7d569729d4804e4f645b90239b59e8956
miko <miko@mikodeMacBook-Pro-2.local>
2024-04-02 15:37:49 -0700

    second

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 import streamlit as st
 import pandas as pd
-import searborn as sns
+import seaborn as sns
 
-st.set_page config(
+st.set_page_config(
     page_title="flower explorer",
     page_icon="💐",
     layout="centered",
+    initial_sidebar_state = "auto",
+    menu_items = None
 )
+
 st.title("💐flower explorer")
+
+df = pd.read_csv('IRIS.csv')
+st.write(df)
